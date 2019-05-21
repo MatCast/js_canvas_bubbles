f9ae7494b7acd56e4ab915aaedf0fa8ff9ce17ef
MatCast <gozyssj4@gmail.com>
2019-05-21 18:10:37 +0200

    Added mouse click interactivity

--- canvas.js
+++ canvas.js
@@ -4,6 +4,7 @@ canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 c = canvas.getContext('2d');
 const angle = Math.PI * 2.0;
+const bubbleCount = 300;
 
 function randColor() {
   let colorsList = ['#FF80E5', '#8093F1', '#F56416', '#FFFC31'];
@@ -20,14 +21,14 @@ function randColor() {
 const mouse = {
   x: undefined,
   y: undefined,
-  prevX: undefined,
-  prevY: undefined,
+  timestamp: undefined,
 };
 
 window.addEventListener('click',
 function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
+  mouse.timestamp = Date.now();
 });
 
 function Bubble(x, y, r, dx, dy, color) {
@@ -53,10 +54,22 @@ function Bubble(x, y, r, dx, dy, color) {
       this.dy = -this.dy;
     }
 
+    // push inside if far
+    if (this.x + this.r > canvas.width + 10 || this.x - this.r < -10) {
+      this.x = Math.random() * canvas.width;
+    }
+
+    if (this.y + this.r > canvas.height + 10 || this.y - this.r < -10) {
+      this.y = Math.random() * canvas.height;
+    }
+
     // interactivity
-    if (mouse.x && this.dx > 0.5) {
-      this.dy = -(this.y - mouse.y) / 10.0;
-      this.dx = -(this.x - mouse.x) / 10.0;
+    if (mouse.x && Date.now() - mouse.timestamp < 1000) {
+      this.dy = -(this.y - mouse.y) / 20.0;
+      this.dx = -(this.x - mouse.x) / 20.0;
+    } else if ((Date.now() - mouse.timestamp > 1000) && (Date.now() - mouse.timestamp < 1010)) {
+      this.dx = (Math.random() < 0.5 ? -1 : 1) * (Math.random() + 0.2) * 1.5;
+      this.dy = (Math.random() < 0.5 ? -1 : 1) * (Math.random() + 0.2) * 1.5;
     }
 
     this.x += this.dx;
@@ -66,23 +79,29 @@ function Bubble(x, y, r, dx, dy, color) {
 }
 
 var bubbles = [];
-for (var i = 0; i < 300; i++) {
-  let x = Math.random() * canvas.width;
-  let y = Math.random() * canvas.width;
-  let dx = (Math.random() - 0.5) * 4;
-  let dy = (Math.random() - 0.5) * 4;
+for (var i = 0; i < bubbleCount; i++) {
   let r = Math.random() * 10;
+
+  // Do not create bubbles on the edge
+  // of the canvas
+  let x = Math.random() * canvas.width - r;
+  x = x < r ? r : x;
+  let y = Math.random() * canvas.height - r;
+  y = y < r ? r : y;
+  let dx = (Math.random() < 0.5 ? -1 : 1) * (Math.random() + 0.2) * 1.5;
+  let dy = (Math.random() < 0.5 ? -1 : 1) * (Math.random() + 0.2) * 1.5;
   color = randColor();
   bubbles.push(new Bubble(x, y, r, dx, dy, color));
 }
 
 function animate() {
   requestAnimationFrame(animate);
+  canvas.width = document.body.clientWidth;
+  canvas.height = document.body.clientHeight;
   c.clearRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < bubbles.length; i++) {
-    bubbles[i].update();
-  }
-
+  bubbles.forEach(function (obj) {
+    obj.update();
+  });
 }
 
-animate(bubbles);
+animate();
